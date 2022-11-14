3ee99d83447329816b6a93a7d0b9dfae213a26c2
kanttanhed <plinio.salazar@yahoo.com.br>
2022-11-13 21:37:48 -0300

    add Post in the controller

---
+++ api/src/main/java/med/salazar/api/controller/MedicoController.java
@@ -0,0 +1,16 @@
+package med.salazar.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medico")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json) {
+        System.out.println(json);
+    }
+}

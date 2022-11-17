2bf6c2ab0405a2921814e4286d82dc80f449629b
kanttanhed <plinio.salazar@yahoo.com.br>
2022-11-17 19:28:23 -0300

    add JPA Entity

--- api/src/main/java/med/salazar/api/controller/MedicoController.java
+++ api/src/main/java/med/salazar/api/controller/MedicoController.java
@@ -1,16 +1,15 @@
 package med.salazar.api.controller;
 
 import med.salazar.api.domain.dtos.DadosCadastroMedico;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medico")
 public class MedicoController {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
         System.out.println(dados);
     }

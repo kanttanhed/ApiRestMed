fa4cba76776dc55078caea5d2671b9c2a21be993
kanttanhed <plinio.salazar@yahoo.com.br>
2022-11-21 23:53:46 -0300

    removing import unusual

--- api/src/main/java/med/salazar/api/repository/MedicoRepository.java
+++ api/src/main/java/med/salazar/api/repository/MedicoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable pagina);
 }

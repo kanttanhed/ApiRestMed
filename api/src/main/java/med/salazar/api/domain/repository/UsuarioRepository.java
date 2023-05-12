69045f13168cd0344a307144417ea1f32ce3fa14
kanttanhed <plinio.salazar@yahoo.com.br>
2023-05-12 19:16:02 -0300

    set up security to be stateless

---
+++ api/src/main/java/med/salazar/api/domain/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package med.salazar.api.domain.repository;
+
+import med.salazar.api.domain.user.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
+}
